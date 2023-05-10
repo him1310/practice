5b01723a1a0a5cf4843bff50fac44a0738c8e231
Himanshu Gupta <him1310@gmail.com>
2023-05-10 08:44:13 +0530

    Modified Techgig1.py

--- TechGig1.py
+++ TechGig1.py
@@ -3,13 +3,17 @@ import sys
 def main():
     N, X = input().split()
     arr = [int(arr) for arr in input().split()]
-    if len(arr) != int(N):
-        sys.exit
-    arr = sorted(arr)
-
-    print(arr[int(N)-int(X)])
+    if int(X) < int (N):
+        arr.sort()
+        if arr[int(N)-int(X)] == arr[int(N)-int(X) - 1]:
+            print(-1)
+        else:
+            print(arr[int(N)-int(X)])
+    else:
+        print(-1)
     # x = [int(x) for x in input().split(" ")]
     # print("Number of list is: ", x)
+    
 
 if __name__ == "__main__":
     main()
