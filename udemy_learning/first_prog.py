ba8416739918f76a989c847233c3c63db7c9aa69
Himanshu Gupta <him1310@gmail.com>
2025-12-25 16:12:23 +0530

    added udemy here

---
+++ udemy_learning/first_prog.py
@@ -0,0 +1,7 @@
+#--- double quotes inside the single quote---
+print('These are the "quotes" from the famour poet')
+print('hello' + ' World!')
+greeting = "Hello"
+name = "Bruce"
+name = input("please enter your name: ")
+print(greeting + ' ' + name)
