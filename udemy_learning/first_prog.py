8c0350548e929e453e28f651ac511dd0904bedd8
Himanshu Gupta <him1310@gmail.com>
2025-12-26 19:39:04 +0530

    added 26-12-2025 learning

--- udemy_learning/first_prog.py
+++ udemy_learning/first_prog.py
@@ -5,3 +5,11 @@ greeting = "Hello"
 name = "Bruce"
 name = input("please enter your name: ")
 print(greeting + ' ' + name)
+print("He is in my custory \" his name is Himanshu \".")
+print("""test " Himanshu " """)
+anotherSplitString = """This is 
+split \
+screen \
+""" # "\" it make the string in different line to behane as one it just gives clarity to the strings
+print(anotherSplitString)
+print("the Number \t in line 1")
