3a297f1ec364e3ab26eb55880415e119f0e66290
Himanshu Gupta <him1310@gmail.com>
2026-01-04 02:27:29 +0530

    draft_cdr_script

--- udemy_learning/learn_module_2.py
+++ udemy_learning/learn_module_2.py
@@ -1,1 +1,19 @@
 # from video 54
+# f = 3.3453456542334523434563465464534
+# print(f.round(2))
+
+IP = "117.131.208.10"
+IP_reversed = ".".join(reversed(IP.split(".")))
+print(IP_reversed)
+
+dd = {
+    "1" : 1,
+    "2" : 2
+}
+c = None
+try: 
+    print(dd[c])
+except KeyError:
+    print("Key_error")
+except Exception:
+    print("test")
